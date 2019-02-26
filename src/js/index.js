026cf41196c9b6194d36f69108c85aa6d29f602e
amandawinkles <47252991+amandawinkles@users.noreply.github.com>
2019-02-26 12:27:04 -0600

    update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,36 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
 
-console.log(`imported ${num}!`);
+/*
+*global state of the app
+*- search object
+*- current recipe object
+*- shopping list object
+*- liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1. get query from view
+    const query = 'pizza'; //to do
+
+    if (query) {
+        // 2. new search object and add to state
+        state.search = new Search(query);
+
+        // 3. prepare UI for results
+
+        // 4. search for recipes
+        await state.search.getResults();
+
+        // 5. render results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+search.getResults();
