2efad7eaf15a2c03a18d801342a9f4b8f96b67ed
amandawinkles <47252991+amandawinkles@users.noreply.github.com>
2019-03-08 10:54:11 -0600

    update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /*
 *global state of the app
@@ -12,7 +14,8 @@ const state = {};
 
 const controlSearch = async () => {
     // 1. get query from view
-    const query = 'pizza'; //to do
+    const query = searchView.getInput();
+    console.log(query);
 
     if (query) {
         // 2. new search object and add to state
@@ -28,9 +31,7 @@ const controlSearch = async () => {
     }
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
-
-search.getResults();
